2e046ebe694c75b5a5f26f6128198f8aae0220a9
Agustín Meliendrez <ameliendrez@gmail.com>
2017-06-09 12:59:35 -0300

    modificacion de ccodigo y ejercicios completos

--- probabilidad_fallos_consecutivos.m
+++ probabilidad_fallos_consecutivos.m
@@ -1,55 +1,42 @@
 
 function [probabilidad, todas_las_probabilidades] = probabilidad_fallos_consecutivos(epsilon)
 
-  % Inicializo las variables que necesito
+  % Inicializo las variables
   probabilidad_anterior = 0; 
   probabilidad_actual = 1;  
   casos_favorables = 0;
-  total_de_casos = 0;
+  cantidad_experimentos = 0;
   todas_las_probabilidades = [];
   
   
   % Mientras el algoritmo no converge
-  while ~converge(probabilidad_anterior, probabilidad_actual, epsilon, total_de_casos)
+   while ~converge(probabilidad_anterior, probabilidad_actual, epsilon, cantidad_experimentos)
   
     % La probabilidad anterior es la probabilidad actual
     probabilidad_anterior = probabilidad_actual;
   
-    % Arrojo la moneda
-    
-    
-    prueba1 = my_mex_service(36626800);
-    
-    if (prueba1 == 0)
+    % Realizo las pruebas
+    prueba1= my_mex_service(36626800);
+    prueba2= my_mex_service(36626800);
       
-      prueba2 = my_mex_service(36626800);
-      
-      if (prueba2 == 0)
-        casos_favorables = casos_favorables + 1;
-        
-       end
-
-    end      
     % Actualizo la cantidad de tiradas
-    total_de_casos = total_de_casos+ 1;
+    cantidad_experimentos = cantidad_experimentos+ 1;
     
-    % Chequeo de qué lado cayó
-    
+    % Se comprueba si cumple la condicion buscada
+    if ((prueba1==0)&&(prueba2)==0)
+      casos_favorables = casos_favorables + 1;
     end
     
     % Calculo la probabilidad actual
-    probabilidad_actual = casos_favorables / total_de_casos;
+    probabilidad_actual = casos_favorables / cantidad_experimentos;
     
     % Esto es solamente para poder hacer el grafico ----------------------------
     todas_las_probabilidades = cat(1, todas_las_probabilidades, probabilidad_actual);
     % --------------------------------------------------------------------------
         
-
+  end
   
   % Devuelvo la probabilidad actual
   probabilidad = probabilidad_actual;
 
 end
-
-
-
--- script_trabajo_especial.m
+++ script_trabajo_especial.m
@@ -2,19 +2,95 @@
 clear 
 clc
 
-epsilon = 0.01;
+epsilon1 = 0.1;
+epsilon2 = 0.01;
+epsilon3 = 0.001;
 
-% Calculamos la probabilidad para sacar cara
-[probabilidad, todas_las_probabilidades] = probabilidad_fallos_consecutivos(epsilon);
+tic
+
+% Calculamos la probabilidad de dos fallos consecutivos
+[probabilidad, todas_las_probabilidades] = probabilidad_fallos_consecutivos(epsilon1);
+
+
 
 % Mostramos por pantalla la probabilidad que nos dio
 fprintf('La probabilidad de exito es: %d\n', probabilidad);
 
-% Graficamos cómo fue evolucionando la probabilidad iteración a iteración
+
+% Graficamos como fue evolucionando la probabilidad 
 figure, plot(todas_las_probabilidades);
-hold on
+hold on;
+grid on;
 plot(ones(size(todas_las_probabilidades)) * 0.5, '--');
 xlabel('Numero de iteracion');
 ylabel('Probabilidad');
 ylim([0 1]);
 legend('Evolucion de la probabilidad', 'Probabilidad analitica');
+
+
+%probabilidad parcial los primeros y ultimos 20
+probabilidad_parcial_primeros_veinte = std(todas_las_probabilidades(1:20))
+probabilidad_parcial_ultimos_veinte = std(todas_las_probabilidades(end-20:end))
+
+
+tiempo= toc;
+fprintf('El tiempo, en segundos, para calcular la probabilidad fue de: %d\n', tiempo);
+
+%comienza la prueba para epsilon 2
+
+tic
+
+% Calculamos la probabilidad de dos fallos consecutivos
+[probabilidad, todas_las_probabilidades] = probabilidad_fallos_consecutivos(epsilon2);
+
+
+
+% Mostramos por pantalla la probabilidad que nos dio
+fprintf('La probabilidad de exito es: %d\n', probabilidad);
+
+% Graficamos como fue evolucionando la probabilidad 
+figure, plot(todas_las_probabilidades);
+hold on;
+grid on;
+plot(ones(size(todas_las_probabilidades)) * 0.5, '--');
+xlabel('Numero de iteracion');
+ylabel('Probabilidad');
+ylim([0 1]);
+legend('Evolucion de la probabilidad', 'Probabilidad analitica');
+
+%probabilidad parcial los primeros y ultimos 20
+probabilidad_parcial_primeros_veinte = std(todas_las_probabilidades(1:20))
+probabilidad_parcial_ultimos_veinte = std(todas_las_probabilidades(end-20:end))
+
+
+tiempo= toc;
+fprintf('El tiempo, en segundos, para calcular la probabilidad fue de: %d\n', tiempo);
+
+%comienza la prueba para epsilon 3
+
+tic
+
+% Calculamos la probabilidad de dos fallos consecutivos
+[probabilidad, todas_las_probabilidades] = probabilidad_fallos_consecutivos(epsilon3);
+
+
+
+% Mostramos por pantalla la probabilidad que nos dio
+fprintf('La probabilidad de exito es: %d\n', probabilidad);
+
+% Graficamos como fue evolucionando la probabilidad 
+figure, plot(todas_las_probabilidades);
+hold on;
+grid on;
+plot(ones(size(todas_las_probabilidades)) * 0.5, '--');
+xlabel('Numero de iteracion');
+ylabel('Probabilidad');
+ylim([0 1]);
+legend('Evolucion de la probabilidad', 'Probabilidad analitica');
+
+%probabilidad parcial los primeros y ultimos 20
+probabilidad_parcial_primeros_veinte = std(todas_las_probabilidades(1:20))
+probabilidad_parcial_ultimos_veinte = std(todas_las_probabilidades(end-20:end))
+
+tiempo= toc;
+fprintf('El tiempo, en segundos, para calcular la probabilidad fue de: %d\n', tiempo);
